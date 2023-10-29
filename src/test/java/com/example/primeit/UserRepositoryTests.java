2e97cc2c47d6171c9c9184ab54a7b5750b9e4f40
fmaliha <fmaliha.maliha9@gmail.com>
2023-10-29 16:11:06 +0600

    -Main Controller -Index -Unit Tests

---
+++ src/test/java/com/example/primeit/UserRepositoryTests.java
@@ -0,0 +1,88 @@
+package com.example.primeit;
+
+import com.example.primeit.user.User;
+import com.example.primeit.user.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+
+//import static junit.framework.Assert.assertNull;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
+public class UserRepositoryTests {
+    @Autowired private UserRepository repo;
+
+    @Test
+    public void testAddNew(){
+        User user = new User();
+        user.setEmail("frzmaliha@gmail.com");
+        user.setPassword("maliha");
+        user.setFirstName("Fairuz");
+        user.setLastName("Maliha");
+
+        User savedUser = repo.save(user);
+
+
+//        assertThat(savedUser,is(notNullValue()));
+//        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+
+        assertNotNull(savedUser);
+        assertTrue(savedUser.getId() > 0);
+
+    }
+    @Test
+    public void testListAll(){
+        Iterable<User> users = repo.findAll();
+        assertThat(users).hasSizeGreaterThan(0);
+
+        for(User user : users)
+        {
+            System.out.println(user);
+        }
+
+    }
+    @Test
+    public void testUpdate(){
+        Integer userId = 1;
+        Optional<User> optionalUser = repo.findById(userId);
+        User user = optionalUser.get();
+        user.setPassword("ichinisan");
+
+        repo.save(user);
+
+        User updatedUser = repo.findById(userId).get();
+        assertThat(updatedUser.getPassword()).isEqualTo("ichinisan");
+    }
+    @Test
+    public void testGet(){
+        Integer userId = 2;
+        Optional<User> optionalUser = repo.findById(userId);
+        assertThat(optionalUser).isPresent();
+        System.out.println(optionalUser.get());
+
+
+    }
+    @Test
+    public void testDelete(){
+        Integer userId = 2;
+        repo.deleteById(userId);
+
+        Optional<User> optionalUser = repo.findById(userId);
+        assertThat(optionalUser).isNotPresent();
+
+    }
+
+}
+
+
